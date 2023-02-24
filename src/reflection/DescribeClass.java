3d2d253f33e98d1e9d5672c8433461dde3c3a54e
Fred Persyn <frederic.persyn@gmail.com>
2023-02-24 01:00:52 +0000

    added q1

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,17 @@
 package reflection;
 
-public class DescribeClass {
-    public static void main(String[] args) {
-        if (args.length != 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+import java.util.Arrays;
 
-        // TODO: implement the functionality for Question 1
+public class DescribeClass {
+    // Write a Java program, `DescribeClass`, that reads the name of a class from the command line and emits
+    // the interface of the class in Java syntax (interface or class, modifiers, constructors, methods,
+    // fields; no method bodies).
+
+    // Hint: You can load a class whose name you know with `java.lang.Class.forName()`. The `java.lang.Class`
+    // class offers a rich interface that enables you to inspect the interface of any class.
+    public static void main(String[] args) throws ClassNotFoundException {
+        if (args.length != 1)
+            System.out.println("Usage: enter a classname argument to get the related interface(s)");
+        System.out.println(Arrays.toString(Class.forName(args[0]).getInterfaces()));
     }
 }
